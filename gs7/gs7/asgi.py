0ab2bf2eb8bbb4b40bc58439f4b3a0c0f5e6e997
SONU <shivkartik1@gmail.com>
2023-02-17 17:47:59 +0530

    dwdw

--- gs7/gs7/asgi.py
+++ gs7/gs7/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for g7 project.
+ASGI config for gs7 project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'g7.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'gs7.settings')
 
 application = get_asgi_application()
--- gs7/gs7/wsgi.py
+++ gs7/gs7/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for g7 project.
+WSGI config for gs7 project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'g7.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'gs7.settings')
 
 application = get_wsgi_application()
