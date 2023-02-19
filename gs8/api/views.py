0bd70f3a48ed7b8b88e5d41f6ceef38713daf875
Sonu <shivkartik1@gmail.com>
2023-02-19 06:03:13 +0530

    create gs8

--- gs8/api/views.py
+++ gs8/api/views.py
@@ -9,7 +9,6 @@ from django.views.decorators.csrf import csrf_exempt
 from django.utils.decorators import method_decorator
 from django.views import View
 
-
 @method_decorator(csrf_exempt,name='dispatch')
 class StudentApi(View):
     def get(self,request,*args,**kwargs):
