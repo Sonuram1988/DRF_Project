89f2696d5de0c7bb7d4cfc2e9cd4c4c6b4d8d55a
Sonu <shivkartik1@gmail.com>
2023-02-21 16:05:10 +0530

    viewset

--- gs14/api/views.py
+++ gs14/api/views.py
@@ -5,37 +5,28 @@ from .serializers import StudentSerializer
 from rest_framework.generics import GenericAPIView
 from rest_framework.mixins import ListModelMixin,CreateModelMixin,RetrieveModelMixin,UpdateModelMixin,DestroyModelMixin
 
-class StudentList(GenericAPIView,ListModelMixin):
+# list and create - pk not required
+class LCStudentAPI(GenericAPIView,ListModelMixin,CreateModelMixin):
     queryset=Student.objects.all()
     serializer_class=StudentSerializer
 
     def get(self,request,*args,**kwargs):
         return self.list(request,*args,**kwargs)
 
-class StudentCreate(GenericAPIView,CreateModelMixin):
-    queryset=Student.objects.all()
-    serializer_class=StudentSerializer
-
     def post(self,request,*args,**kwargs):
         return self.create(request,*args,**kwargs)
 
-class StudentRetrieve(GenericAPIView,RetrieveModelMixin):
+#put,retrieve,destroy- pk required
+class RDPStudentAPI(GenericAPIView,RetrieveModelMixin,DestroyModelMixin,UpdateModelMixin):
+
     queryset=Student.objects.all()
     serializer_class=StudentSerializer
 
     def get(self,request,*args,**kwargs):
         return self.retrieve(request,*args,**kwargs)
 
-class StudentUpdate(GenericAPIView,UpdateModelMixin):
-    queryset=Student.objects.all()
-    serializer_class=StudentSerializer
-
     def put(self,request,*args,**kwargs):
         return self.update(request,*args,**kwargs)
-
-class StudentDelete(GenericAPIView,DestroyModelMixin):
-    queryset=Student.objects.all()
-    serializer_class=StudentSerializer
 
     def delete(self,request,*args,**kwargs):
         return self.destroy(request,*args,**kwargs)
--- gs14/gs14/urls.py
+++ gs14/gs14/urls.py
@@ -4,6 +4,7 @@ from api import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('studentapi/',views.student_api),
-    path('studentapi/<int:pk>/',views.student_api),
+    path('studentapi/',views.LCStudentAPI.as_view()),
+    path('studentapi/<int:pk>/',views.RDPStudentAPI.as_view()),
+   
 ]
