5cbf4ccfb10777b7be75290a79a082795cf6db2a
Sonu <shivkartik1@gmail.com>
2023-02-24 05:11:32 +0530

    custom auth

---
+++ gs27/api/views.py
@@ -0,0 +1,16 @@
+from .models import Student
+from .serializers import StudentSerializer
+from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticated
+
+
+class StudentModelViewSet(viewsets.ModelViewSet):
+  queryset=Student.objects.all()
+  serializer_class=StudentSerializer
+  
+  
+  
+
+
+
+  
