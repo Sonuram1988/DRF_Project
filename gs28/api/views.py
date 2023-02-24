407f0618218baea84ad3ca740e8ed37d7f6674c9
Sonu <shivkartik1@gmail.com>
2023-02-24 10:54:04 +0530

    jwt create token

--- gs28/api/views.py
+++ gs28/api/views.py
@@ -3,12 +3,13 @@ from .serializers import StudentSerializer
 from rest_framework import viewsets
 from rest_framework.permissions import IsAuthenticated
 from api.customauth import CustomAuthentication
+from rest_framework_simplejwt.authentication import JWTTokenUserAuthentication
 
 
 class StudentModelViewSet(viewsets.ModelViewSet):
   queryset=Student.objects.all()
   serializer_class=StudentSerializer
-  authentication_classes=[CustomAuthentication]
+  authentication_classes=[JWTTokenUserAuthentication]
   permission_classes=[IsAuthenticated]
 
   
